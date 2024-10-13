573dcfa2a66ba67515928d13ed0e10ef1ee7fead
leesu9042 <tnckddl8957@gmail.com>
2024-10-13 14:46:48 +0900

    4주차 완료

--- product/urls.py
+++ product/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:content_id>/', views.detail, name='product_detail'),
+    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
+    path('comment/update/<int:comment_id>/', views.comment_update, name='comment_update'),
+    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
+
 
 ]
